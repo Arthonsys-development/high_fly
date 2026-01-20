6516971d6804659621724cdd6095fa110f150a43
javed786786 <javed.khan@arthonsys.com>
2026-01-20 10:35:03 +0530

    update app version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
         applicationId = "vistarak.apps"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.1"
         multiDexEnabled = true
     }
